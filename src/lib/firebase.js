7fece5498238c9523cef25703a536d1af4318f22
innocentdavid <paulinnocent04@gmail.com>
2022-07-07 18:06:53 +0100

    cent-update

---
+++ src/lib/firebase.js
@@ -0,0 +1,22 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+
+// Initialize Firebase
+export const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MESSUREMENT_ID
+};
+
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+export var analytics = undefined;
+if (app?.name && typeof window !== 'undefined') {
+  analytics = getAnalytics(app)
+}
+export const auth = getAuth();
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,31 @@
 import "../styles/globals.css";
 import "tailwindcss/tailwind.css";
+import { useEffect } from 'react';
 import { AppProvider } from "../Layout/context";
+import { analytics } from '../lib/firebase';
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }) {
+  const routers = useRouter();
+
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'production' && analytics) {
+      const logEvent = (url) => {
+        analytics().setCurrentScreen(url);
+        analytics().logEvent('screen_view');
+      };
+
+      routers.events.on('routeChangeComplete', logEvent);
+      //For First Page
+      logEvent(window.location.pathname);
+
+      //Remvove Event Listener after un-mount
+      return () => {
+        routers.events.off('routeChangeComplete', logEvent);
+      };
+    }
+  }, [])
+
   return (
     <>
       <AppProvider>
