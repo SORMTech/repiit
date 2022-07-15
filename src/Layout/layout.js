2c17c04039eb8d681f53f908e89197d9eebdfc83
Chosen2730 <70141158+Chosen2730@users.noreply.github.com>
2022-07-15 10:16:44 +0100

    added login

--- src/Layout/layout.js
+++ src/Layout/layout.js
@@ -4,7 +4,7 @@ import Footer from "./footer";
 
 const Layout = ({ children }) => {
   return (
-    <div>
+    <div className='max-w-7xl mx-auto'>
       <Nav />
       {children}
       <Footer />
