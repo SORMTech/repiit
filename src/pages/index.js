208a61b47afdfc7f4983387554e4c5d2805bb684
innocentdavid <paulinnocent04@gmail.com>
2022-08-01 14:47:36 +0100

    t2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,14 @@
 import { useGlobalContext } from "../Layout/context";
-// import Layout from "./../Layout/layout";
-// import Head from "./../Layout/head";
-// import Hero from "../components/home/hero";
-// import Main from "../components/home/main";
+import Layout from "./../Layout/layout";
+import Head from "./../Layout/head";
+import Hero from "../components/home/hero";
+import Main from "../components/home/main";
 import { useEffect } from "react";
-// import { getProductsByPropty } from "../utils/getData";
+import { getProductsByPropty } from "../utils/getData";
 // import { useAuth } from "../context/AuthContext";
 
 export default function Home({ featuredProducts, trendingProducts }) {
   // const { user } = useAuth()
-  const user = null
   const { name } = useGlobalContext();
 
   useEffect(() => {
@@ -17,22 +16,22 @@ export default function Home({ featuredProducts, trendingProducts }) {
   }, [featuredProducts, trendingProducts])
 
   return (
-    <>hello
-      {/* <Layout user={user} >
+    <>
+      <Layout user={null} >
         <Head />
         <Hero />
         <Main featuredProducts={featuredProducts} trendingProducts={trendingProducts} />
-      </Layout> */}
+      </Layout>
     </>
   );
 }
 
 export async function getStaticProps() {
-  // const featuredProducts = await getProductsByPropty('featured', 4)
-  // const trendingProducts = await getProductsByPropty('trending', 4)
+  const featuredProducts = await getProductsByPropty('featured', 4)
+  const trendingProducts = await getProductsByPropty('trending', 4)
 
   return {
-    props: { featuredProducts: null, trendingProducts: null },
+    props: { featuredProducts, trendingProducts },
     revalidate: 1
   }
 }
